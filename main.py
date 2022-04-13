9f9655afc3dd4b5984d7876dc731e2631a557d4a
Monalisa Egobishi <55362351+monalisa-effect@users.noreply.github.com>
2022-04-13 08:16:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+age = 90 - int(age)
+print(f"You have {365*age}days, {52*age}weeks, {12*age}months left")
+
+
+
+
+
+
+
+
